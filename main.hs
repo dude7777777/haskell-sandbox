31922aa01d018d9d49521d3a03f0d6d4bc48dacf
Jacob Preston <jacobppreston@gmail.com>
2021-12-21 19:04:13 -0700

    basic data types

---
+++ main.hs
@@ -0,0 +1,18 @@
+import Data.List
+import System.IO
+
+-- Int -2^63 2^63
+maxInt = maxBound :: Int
+minInt = minBound :: Int
+
+-- Double
+bigFloat = 3.99999999999 + 0.00000000005
+
+-- Bool True False
+-- Char '
+-- Tuple 
+
+always5 :: Int
+always5 = 5
+
+helloWorld = "Hello World"
